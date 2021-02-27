556af2b55f717bb44b8d6abd2e11ba2191fbb71b
tdchien88 <tdchien@brights.vn>
2021-02-27 23:30:17 +0900

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from "react";
-import "./style.css";
+import { Column, Row } from "simple-flexbox";
+import { StyleSheet, css } from "aphrodite";
+import SidebarComponent from "./components/sidebar/SidebarComponent";
+import HeaderComponent from "./components/header/HeaderComponent";
+import "./App.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+const styles = StyleSheet.create({
+  container: {
+    height: "100vh"
+  },
+  content: {
+    marginTop: 54
+  },
+  mainBlock: {
+    backgroundColor: "#F7F8FC",
+    padding: 30
+  }
+});
+
+class App extends React.Component {
+  state = { selectedItem: "Tickets" };
+
+  render() {
+    const { selectedItem } = this.state;
+    return (
+      <Row className={css(styles.container)}>
+        <SidebarComponent
+          selectedItem={selectedItem}
+          onChange={selectedItem => this.setState({ selectedItem })}
+        />
+        <Column flexGrow={1} className={css(styles.mainBlock)}>
+          <HeaderComponent title={selectedItem} />
+          <div className={css(styles.content)}>
+            <span>Content</span>
+          </div>
+        </Column>
+      </Row>
+    );
+  }
 }
+
+export default App;
